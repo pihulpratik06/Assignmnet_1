0a94aa8e06ea18c5162556e9510d5fb0a60ae680
pihulpratik06 <99160122+pihulpratik06@users.noreply.github.com>
2022-03-07 16:45:24 +0530

    Add files via upload

---
+++ Login/src/com/Demi/LoginCheck.java
@@ -0,0 +1,61 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginCheck
+ */
+//@WebServlet("/LoginCheck")
+public class LoginCheck extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginCheck() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		PrintWriter out=response.getWriter();
+		
+		String uname=request.getParameter("uname");
+		
+		String pass=request.getParameter("pass");
+		
+		if(uname.equals("java") && pass.equals("1234")) {
+			 
+			out.println("Login Succesfull");
+			
+		}
+		else {
+			
+			out.println("Wrong username or password");
+			
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
