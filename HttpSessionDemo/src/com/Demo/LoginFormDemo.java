b58f599ac025cf11d19df025572122c9ca3734fc
pihulpratik06 <99160122+pihulpratik06@users.noreply.github.com>
2022-03-09 13:57:47 +0530

    Add files via upload

---
+++ HttpSessionDemo/src/com/Demo/LoginFormDemo.java
@@ -0,0 +1,77 @@
+package com.Demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+@WebServlet("/LoginFormDemo")
+public class LoginFormDemo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginFormDemo() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+   
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+   
+         response.setContentType("text/html");
+        
+        PrintWriter out=response.getWriter();
+	   
+        String uname=request.getParameter("uname");
+      
+        String pass=request.getParameter("pwd");
+         
+         HttpSession session=request.getSession();
+        
+          session.setAttribute("uname", uname); 
+         
+          
+         
+        
+         if(uname.equals(pass)) {
+        	  
+        	 out.println("<h1>Right Credential</h1>");
+        	 RequestDispatcher rd=request.getRequestDispatcher("WelcomeServlet");
+        	  rd.forward(request, response);
+        	 
+         }
+         else {
+        	  
+        	
+        	 
+        	 RequestDispatcher rd=request.getRequestDispatcher("index.html");
+        	 rd.include(request, response);
+        	  
+         }
+        
+     
+     
+     
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
