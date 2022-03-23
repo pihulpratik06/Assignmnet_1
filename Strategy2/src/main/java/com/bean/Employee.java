d3b99453cb6846476e98f04085eda92c09f00311
pihulpratik06 <99160122+pihulpratik06@users.noreply.github.com>
2022-03-23 15:10:30 +0530

    Add files via upload

---
+++ Strategy2/src/main/java/com/bean/Employee.java
@@ -0,0 +1,46 @@
+//Using Annotation
+
+package com.bean;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="EmployeeTPS")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
+
+     @Column(name="NameOfEmployee")
+    private String name;
+
+    //Constructor
+
+    public Employee(int id, String name) {
+
+        this.id = id;
+        this.name = name;
+
+    }
+
+    //Setter and Getter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
---
+++ Strategy2/src/main/java/com/bean/FullTimeEmp.java
@@ -0,0 +1,49 @@
+//Using Annotation
+
+package com.bean;
+import com.bean.Employee;
+
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TPSFullTime")
+@PrimaryKeyJoinColumn(name="ID")
+public class FullTimeEmp extends Employee {
+
+    private int sal;
+    private int extra_hrs;
+    private int extra_sal;
+
+    public FullTimeEmp(int id, String name, int sal, int extra_hrs, int extra_sal) {
+        super(id, name);
+        this.sal = sal;
+        this.extra_hrs = extra_hrs;
+        this.extra_sal = extra_sal;
+    }
+
+    public int getSal() {
+        return sal;
+    }
+
+    public void setSal(int sal) {
+        this.sal = sal;
+    }
+
+    public int getExtra_hrs() {
+        return extra_hrs;
+    }
+
+    public void setExtra_hrs(int extra_hrs) {
+        this.extra_hrs = extra_hrs;
+    }
+
+    public int getExtra_sal() {
+        return extra_sal;
+    }
+
+    public void setExtra_sal(int extra_sal) {
+        this.extra_sal = extra_sal;
+    }
+}
